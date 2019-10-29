7976e691b8496ecf6c204f8c700d6797c18f6d1f
yannisf <yannisf@ouranos>
2019-10-29 21:22:21 +0200

    Script to prepare the provisioning

--- prepare.sh
+++ prepare.sh
@@ -3,6 +3,12 @@
 JDK=OpenJDK8U-jdk_x64_linux_hotspot_8u232b09.tar.gz
 SPARK=spark-2.4.4-bin-hadoop2.7.tgz
 
+wget https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u232-b09/$JDK
+
+wget http://apache.otenet.gr/dist/spark/spark-2.4.4/$SPARK
+
+ssh-keygen -f vmcluster.id_rsa -P ""
+
 cp $JDK vm2
 cp $SPARK vm2
 cp vmcluster.id_rsa* vm2
